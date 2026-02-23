c47142a05663a9eddc3f894dc6eee8b801ccaede
Fajrisidiqx <fipehep552@roratu.com>
2026-02-23 15:17:41 +0700

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className=" bg-white w-full py-6 text-center text-sm text-gray-400">
-      © {new Date().getFullYear()} CariHadist
+      © {new Date().getFullYear()} CariHadits
     </footer>
   );
 }
